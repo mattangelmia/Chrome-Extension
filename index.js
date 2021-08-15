73325fa29bcc1468fd55969fd3738dd2bc131dc7
mattangelmia <23matthewangel@gmail.com>
2021-08-15 19:11:32 -0400

    added functionality that lets you  save and delete urls from extension

--- index.js
+++ index.js
@@ -1,61 +1,44 @@
 // Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
-let myLeads = []
-const button = document.querySelector("#input-btn")
-const inputEl = document.querySelector("#input-el")
-// let inputText = inputEl.value
-const ulEl = document.querySelector("#ul-el")
+let myLeads = [];
+const inputEl = document.querySelector("#input-el");
+const inputBtn = document.querySelector("#input-btn");
+const ulEl = document.querySelector("#ul-el");
+const deleteBtn = document.querySelector("#delete-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+console.log(leadsFromLocalStorage);
+const saveTabBtn = document.querySelector("#tab-btn");
 
-
-button.addEventListener("click", addText)
-
-function addText() {
-console.log(inputEl.value)
-myLeads.push(inputEl.value)
-console.log(myLeads)
-render()
-
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
 }
 
-
-function render(){
-  
-    let listItems = ""
-    for(let i = 0; i < myLeads.length; i++) {
-        // console.log(myLeads[i])
-        listItems += "<li>"   + myLeads[i] + "</li>"
-        ulEl.innerHTML = listItems
-        //   create element
-        // set text content
-        // append to ul
-    //    let li =  document.createElement('li')
-    //    li.textContent = myLeads[i]
-    //    console.log( li.textContent)
-    //    ulEl.appendChild(li);
-       
-    }
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    // console.log(myLeads[i])
+    let a = document.createElement("a");
+    a.setAttribute("href", myLeads[i]);
+    a.innerText = myLeads[i];
+    //    listItems += "<li><a target='_blank' href=' " + myLeads[i] + " '>" + myLeads[i] + "</a></li>"
+    listItems += `<li>
+    <a target='_blank' href='${leads[i]}'>${leads[i]}</a>
+    </li>`;
+    console.log(a);
+  }
+  ulEl.innerHTML = listItems;
 }
 
+deleteBtn.addEventListener("dblclick", function () {
+  console.log("click");
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
 
-
-
-
-
-
-
-
-
-
-
-// const container = document.getElementById("container")
-// container.innerHTML = "<button>Buy!</button>"
-// const button  = document.querySelector("button") 
-// // When clicked, render a paragraph under the button (in the container)
-// // that says "Thank you for buying!"
-// console.log(button)
-// button.addEventListener('click', buy)
-// // function renderPara(){
-// //     container.innerHTML += "<p>Thank you for buying!</p>" 
-// // }
-// function buy(){
-//     container.innerHTML += "<p>Thank you for buying!</p>"
-// }
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
