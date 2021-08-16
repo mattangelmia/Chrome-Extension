9754f0abb87d8c4bb7e800f3e42a3cc0e7ca9885
mattangelmia <23matthewangel@gmail.com>
2021-08-15 20:27:31 -0400

    added save tab

--- index.js
+++ index.js
@@ -6,12 +6,21 @@ const ulEl = document.querySelector("#ul-el");
 const deleteBtn = document.querySelector("#delete-btn");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 console.log(leadsFromLocalStorage);
-const saveTabBtn = document.querySelector("#tab-btn");
+const tabBtn = document.querySelector("#tab-btn");
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   render(myLeads);
 }
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    console.log(tabs[0].url);
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
 
 function render(leads) {
   let listItems = "";
